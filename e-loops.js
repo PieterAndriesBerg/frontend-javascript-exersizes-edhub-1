cf72b4af6ec9e87f4a37d468354c5c637434da95
Pieter-Andries van der Berg <p-a.berg@outlook.com>
2020-10-16 12:02:50 +0200

    made assignments

--- e-loops.js
+++ e-loops.js
@@ -1,7 +1,9 @@
 // ==========================================
 // 1. Maak een for-loop die 3 keer "Hoera!" in de console logt
 // ==========================================
-
+for (let i = 0; i < 3; i++) {
+    console.log("Hoera!");
+}
 
 // ==========================================
 // 2. Maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
@@ -18,6 +20,19 @@
 // >> 7
 // >> 9
 // ==========================================
+for (let i = 0; i < 10; i++) {
+    if (i < 3) {
+        console.log(i);
+    }
+    if (i < 6 && i >= 3) {
+        console.log("> " + i);
+    }
+
+    if (i >=6) {
+        console.log(">> " +i);
+    }
+}
+
 
 
 // ==========================================
@@ -33,4 +48,10 @@
 // tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over..
 // hier bestaat een speciale operator voor
 // ==========================================
-
+for (let i = 0; i < 16; i++) {
+    if ( i % 2 === 0 ){
+        console.log("Even");
+    } else {
+        console.log("Odd");
+    }
+}
