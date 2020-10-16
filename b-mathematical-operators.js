3f65810f7665e99fbe77bce469662bcf97720168
Pieter-Andries van der Berg <p-a.berg@outlook.com>
2020-10-16 11:18:07 +0200

    made assignments

--- b-mathematical-operators.js
+++ b-mathematical-operators.js
@@ -5,34 +5,45 @@
 // ==========================================
 // 1. Declareer een variabele en sla daar de uitkomst van 4 plus 5 in op.                           // geeft 9
 // ==========================================
-
+const plus = 4 + 5;
+console.log(plus)
 
 // ==========================================
 // 2. Declareer een variabele en sla daar de uitkomst van 7 maal 52 in op.                          // geeft 364
 // ==========================================
-
+const keer = 7 * 52;
+console.log(keer)
 
 // ==========================================
 // 3. Declareer een variabele en sla daar de uitkomst van 4 + 3 in op, vermenigvuldigd met 7.       // geeft 49
 // ==========================================
-
+const plus_keer = (4 + 3) * 7;
+console.log(plus_keer);
 
 // ==========================================
 // 4. Declareer een variabele en sla daar de uitkomst van 36 gedeeld door 6, maal 2 in op.          // geeft 12
 // ==========================================
-
+const delen = 36 / 6 * 2;
+console.log(delen);
 
 // ==========================================
 // 5. Declareer twee variabelen en zet daarin respectievelijk de waardes 'zoet' en 'sappig'.
 // Declareer nog een variabele en sla daarin de samenvoeging van bovenstaande variabelen in op.     // geeft 'zoetsappig'
 // ==========================================
-
+const zoet = "zoet";
+const sappig = "sappig";
+const zoetsappig = zoet + sappig;
+console.log(zoetsappig);
 
 // ==========================================
 // 6. Declareer een variabele voor jouw voornaam en een variabele voor jouw achternaam en zet de waardes erin.
 // Declareer vervolgens een variabele voor jouw volledige naam en plak bovenstaande variabelen aan elkaar.
 // Zorg dat er een spatie tussen zit zonder iets aan de originele variabelen te veranderen.        // geeft bijv. 'Henk Pieters'
 // ==========================================
+const voornaam = "Pieter-Andries";
+const achternaam = "van der Berg";
+const volledigeNaam = voornaam + " " + achternaam;
+console.log(volledigeNaam);
 
 
 // ==========================================
@@ -40,3 +51,7 @@
 // Declareer vervolgens een variabele voor jouw locatie en plak jouw de woonplaats en provincie aan elkaar.
 // Zorg dat er een komma en spatie tussen zit zonder iets aan de originele variabelen te veranderen. // geeft bijv. Maastricht, Limburg
 // ==========================================
+const woonplaats = "Sveen";
+const provincie = "Friesland";
+const locatie = woonplaats + ", " + provincie;
+console.log(locatie);
